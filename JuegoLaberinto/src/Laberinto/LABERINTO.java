488a75581f28d1d78266f5697765d1f6ce82c8e1
Tania024 <tania01lojano@hotmail.com>
2021-08-04 18:53:36 -0500

    Laberinto

---
+++ JuegoLaberinto/src/Laberinto/LABERINTO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Laberinto;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class LABERINTO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PANEL_LABERINTO pa = new PANEL_LABERINTO();
+        pa.setVisible(true);
+    }
+    
+}
